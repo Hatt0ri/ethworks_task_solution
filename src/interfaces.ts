17b462fbc6a74982970f5b2a385b4cab70cb0e67
Karol Pajor <kp.pajor@gmail.com>
2020-07-15 13:54:44 +0200

    feat: parseInput

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,4 +3,8 @@ export interface ITerm {
     exponent: number;
 }
 
+export interface IHashMap {
+    [key: number]: ITerm;
+}
+// the first is a coefficient num and the second is an exponent num
 export type IInputTerm = [number, number];
--- src/sumSequences.ts
+++ src/sumSequences.ts
@@ -1,9 +1,33 @@
-import { IInputTerm } from './interfaces';
+import { IInputTerm, IHashMap } from './interfaces';
+import { get } from 'lodash';
 
 export const sumSequences = (firstSequence: IInputTerm[], secondSequence: IInputTerm[]): string => {
+    if (isInputInvalid(firstSequence, secondSequence)) {
+        return null;
+    }
 
-    // TODO
+    const hashMap: IHashMap = {};
+    parseInput(firstSequence, secondSequence, hashMap);
 
-    return '';
+
+    return null;
+};
+
+const isInputInvalid = (firstSequence: IInputTerm[], secondSequence: IInputTerm[]) => {
+    return get(firstSequence, 'length', 0) === 0 || get(secondSequence, 'length', 0) === 0;
+};
+
+const parseInput = (firstSequence: IInputTerm[], secondSequence: IInputTerm[], hashMap: IHashMap) => {
+    firstSequence.forEach((term) => {
+        hashMap[term[1]] = { coefficient: term[0], exponent: term[1] };
+    });
+    secondSequence.forEach((term) => {
+        if (typeof hashMap[term[1]] === 'undefined') {
+        hashMap[term[1]] = { coefficient: term[0], exponent: term[1] };
+        } else {
+            const coefficient = hashMap[term[1]].coefficient;
+            hashMap[term[1]] = { ...hashMap[term[1]], coefficient: coefficient + term[0] };
+        }
+    });
 };
 
