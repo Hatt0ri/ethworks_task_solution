622c231702d3de6c81669b15a7a1ab724c888c49
Karol Pajor <kp.pajor@gmail.com>
2020-07-15 13:53:50 +0200

    test(sumSequences): do simple test

--- src/spec.ts
+++ src/spec.ts
@@ -1,8 +1,15 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+import { sumSequences } from './sumSequences';
+import { expect } from 'chai';
+
+describe('Tests of sumSequences()', () => {
+  describe('#sumSequences()', () => {
+    it('should return null for empty sequences', () => {
+      const sum = sumSequences([],[]);
+      expect(sum).to.equal(null);
+    });
+    it('should return "3x^3 + 3x^2 + 3"', () => {
+      const sum = sumSequences([[2, 2], [3, 0]], [[3, 3], [1, 2]]);
+      expect(sum).to.equal('3x^3 + 3x^2 + 3');
     });
   });
 });
